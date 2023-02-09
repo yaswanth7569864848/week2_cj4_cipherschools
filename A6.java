194cabac15b791d7f066763a55c7986337b46e70
yaswanth7569864848 <104352479+yaswanth7569864848@users.noreply.github.com>
2023-02-09 20:25:21 +0530

    Add files via upload

---
+++ A6.java
@@ -0,0 +1,27 @@
+package CoreJava;
+
+class a {
+    final int nopfalphabate = 5;
+    final int noofdigit;
+
+    a() {
+        noofdigit = 10;
+    }
+
+    final void fun() {
+        System.out.println("Hi, i am function in class A");
+    }
+}
+
+class B extends A {
+    // void fun() {
+    // System.out.println("Hi, i am function in class A");
+    // }
+}
+
+// class C extends B{
+//
+// }
+public class A6 {
+
+}
---
+++ A7.java
@@ -0,0 +1,48 @@
+class A {
+    int a, b;
+
+    public void show() {
+        System.out.println("Hello!");
+    }
+}
+
+// class B extends A{
+// public void speak() {
+// System.out.println("Writing!");
+// }
+// }
+
+interface b {
+    public void eat();
+}
+
+interface D extends b {
+    public void sleep();
+}
+
+class C implements b, D {
+    int e, f;
+
+    public void speak() {
+        System.out.println("Speaking");
+    }
+
+    @Override
+    public void eat() {
+
+    }
+
+    @Override
+    public void sleep() {
+
+    }
+}
+
+public class A7 {
+
+    public static void main(String[] args) {
+        // B b = new B();
+        // C c = new C();
+    }
+
+}
